4760382720e767480dd35ad5687dc184721c6ab6
youssefsoli <youssefdevelops@gmail.com>
2023-01-22 07:17:10 -0500

    Added some files

--- filetype_to_color.py
+++ filetype_to_color.py
@@ -2,41 +2,18 @@ import json
 import numpy as np
 from sklearn.neighbors import NearestNeighbors
 import math
+import seaborn as sns
 
 
 if __name__ == "__main__":
-    data = json.load(open('./3D-coordinates.json'))
-    texts = data['texts'][:-1]
-    embeddings = np.array(data['embeddings'][:-1])
-    hashes = open('./hashes.txt').read().split()[2::3]
+    # data = json.load(open('graph_data.json'))
+    #
+    # file_types = set()
+    # for node in data['nodes']:
+    #     type_entry = node['name'].split(', ')[-1]
+    #     type = type_entry.split(': ')[1]
+    #     file_types.add(type)
+    #
+    # print(file_types)
 
-    nodes = [{
-        "id": hashes[i],
-        "name": texts[i],
-        "coord": embeddings[i]
-    } for i in range(len(hashes))]
-
-    links = []
-
-    neighbors = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(embeddings)
-    distances, indices = neighbors.kneighbors(embeddings)
-
-    for level, index in enumerate(indices):
-        i, j = index
-        links.append({
-            "source": nodes[i]['id'],
-            "target": nodes[j]['id']
-        })
-
-        nodes[i]['val'] = math.ceil(1/(distances[level][1]))
-        # No longer need the coordinate
-        del nodes[i]['coord']
-
-    json_data = {
-        "nodes": nodes,
-        "links": links
-    }
-
-    with open("graph_data.json", "w") as f:
-        json.dump(json_data, f)
-
+    print(sns.color_palette('muted'))
